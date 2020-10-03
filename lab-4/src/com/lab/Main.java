b74e6d0fbb802094bf507ad876e774389032d03c
Oleksii Luppa <asleep.alex@gmail.com>
2020-10-03 21:15:53 +0300

    Code refractoring

--- lab-4/src/com/lab/Main.java
+++ lab-4/src/com/lab/Main.java
@@ -7,6 +7,12 @@ public class Main {
 
     public static void main(String[] args) {
         int[] arr = {5, 3, 2, 8, 1, 4};
+        arr = Sorter.sortArray(arr);
+    }
+}
+
+class Sorter {
+    public static int[] sortArray(int[] arr){
         ArrayList<Integer> odd = new ArrayList<Integer>();
         for(int i = 0; i < arr.length; i++){
             if(arr[i] % 2 == 1){
@@ -26,5 +32,6 @@ public class Main {
         for(int i = 0; i < arr.length; i++){
             System.out.println(arr[i]);
         }
+        return arr;
     }
 }
