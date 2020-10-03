c015e28be397f07cd81daa081b0e00456b2e7201
Oleksii Luppa <asleep.alex@gmail.com>
2020-10-03 21:50:39 +0300

    Task 2

--- lab-4/src/com/lab/Main.java
+++ lab-4/src/com/lab/Main.java
@@ -6,13 +6,19 @@ import java.util.Collections;
 public class Main {
 
     public static void main(String[] args) {
+        //Task 1
         int[] arr = {5, 3, 2, 8, 1, 4};
-        arr = Sorter.sortArray(arr);
+        arr = Sorter.sortArrayOnlyOdds(arr);
+        System.out.println("____________");
+        //Task 2
+        int[] unique = {2, 2, 1, 1, 2, 2, 5};
+        int result = Sorter.findUnique(unique);
+        System.out.println(result);
     }
 }
 
 class Sorter {
-    public static int[] sortArray(int[] arr){
+    public static int[] sortArrayOnlyOdds(int[] arr){
         ArrayList<Integer> odd = new ArrayList<Integer>();
         for(int i = 0; i < arr.length; i++){
             if(arr[i] % 2 == 1){
@@ -33,5 +39,16 @@ class Sorter {
             System.out.println(arr[i]);
         }
         return arr;
+    }
+
+    public static int findUnique(int[] inputArray)
+    {
+        int result = 0;
+        for(int i=0;i<inputArray.length;i++)
+        {
+            result ^= inputArray[i];
+        }
+
+        return (result > 0 ? result : -1);
     }
 }
